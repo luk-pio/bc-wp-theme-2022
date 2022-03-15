e0fdf35f423d23056f3a49ce46b8b1ee80567396
LukaszPiotrakArdigen <lukasz.piotrak@ardigen.com>
2022-03-15 20:45:28 +0100

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Setup Theme
+ */
+function setup()
+{
+    add_theme_support("post-thumbnails");
+}
+add_action("after_setup_theme", "setup");
+
+function EnqueueMyStyles() {
+    // wp_enqueue_style('my-custom-style', get_template_directory_uri() . '/css/my-custom-style.css', false, '20150320');
+
+    wp_enqueue_style('my-main-style', get_stylesheet_uri(), false, '20150320');
+}
+add_action('wp_enqueue_scripts', 'EnqueueMyStyles');
