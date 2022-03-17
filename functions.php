362ce6de03a816ea1e1921f4dfd1344cbe87e1cd
LukaszPiotrakArdigen <lukasz.piotrak@ardigen.com>
2022-03-17 12:25:57 +0100

    Add sketch of scss flexbox order

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function setup()
 add_action("after_setup_theme", "setup");
 
 function EnqueueMyStyles() {
-    // wp_enqueue_style('my-custom-style', get_template_directory_uri() . '/css/my-custom-style.css', false, '20150320');
+    wp_enqueue_style('my-custom-style', get_template_directory_uri() . '/css/my-custom-style.css', false, '20150320');
 
     wp_enqueue_style('my-main-style', get_stylesheet_uri(), false, '20150320');
 }
