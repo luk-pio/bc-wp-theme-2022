7671f4051a2a7d0116cbb9186dd193a7aae931f9
LukaszPiotrakArdigen <lukasz.piotrak@ardigen.com>
2022-04-03 00:09:08 +0200

    feat: add social and header icons

---
+++ static/icons/Icon-Menu.svg.php
@@ -0,0 +1,5 @@
+<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M24 5.44118H0V4.5H24V5.44118Z" fill="#070707"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M24 12.9705H0V12.0293H24V12.9705Z" fill="#070707"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M24 20.4998H0V19.5586H24V20.4998Z" fill="#070707"/>
+</svg>
